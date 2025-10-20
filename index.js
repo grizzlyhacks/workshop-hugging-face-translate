41a8c59cdb0e7c4c12736ec324ca48f785959e9e
Cengiz Gunay <cengique@users.sf.net>
2025-10-20 19:56:07 -0400

    updated gradio connection string

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { client } from "@gradio/client";
 
-const app = await client("https://gradio-tests-english-to-spanish.hf.space/");
+const app = await client("gradio-tests-old/english_to_spanish");
 const result = await app.predict("/predict", [		
 				"Howdy!", // string  in 'text' Textbox component
 	]);
